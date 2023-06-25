875261eb078a0d9b5814999d6fd41c873c7b2f3e
ddrcdev <david.del.rio19@gmail.com>
2023-06-25 13:56:10 +0200

    Update registerEnroll.sh

--- universidades/organizations/fabric-ca/registerEnroll.sh
+++ universidades/organizations/fabric-ca/registerEnroll.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-function createUniversity(NAME,PORT) {
+function createUniversity() {
+  local NAME=$1  # Definir la variable NAME usando el primer argumento de la función
+  local PORT=$2  # Definir la variable PORT usando el segundo argumento de la función
+
   infoln "Enrolling the CA admin"
   mkdir -p organizations/peerOrganizations/${NAME}.universidades.com/
 
