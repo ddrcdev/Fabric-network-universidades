97ce5ec93fbaece04723b61ec3392f5bb16d6db6
ddrcdev <david.del.rio19@gmail.com>
2023-06-25 23:45:51 +0200

    Update config-univ-network.sh

--- config-univ-network.sh
+++ config-univ-network.sh
@@ -1,7 +1,10 @@
+
 #######################################
-########## REQUISITOS PREVIOS #########
+###### ELIMINACIÓN INSTALACIONES ######
+#######         PREVIAS          ######
 #######################################
 
+cd 
 sudo rm -rf fabric-universidades-iebs
 #Reinicio de contenedores
 docker stop $(docker ps -a -q)
@@ -9,6 +12,18 @@ docker rm $(docker ps -a -q)
 docker volume prune
 docker network prune
 
+sudo apt-get purge docker-ce docker-ce-cli containerd.io
+sudo rm -rf /var/lib/docker
+sudo rm -rf /etc/docker
+sudo rm -rf /var/lib/docker/volumes
+sudo rm -rf /etc/default/docker
+sudo rm -rf /etc/sysconfig/docker
+sudo rm /usr/local/bin/docker-compose
+
+
+#######################################
+########## REQUISITOS PREVIOS #########
+#######################################
 
 #Comprobación de versiones
 git version
@@ -69,6 +84,9 @@ rm -rf channel-artifacts/
 
 #Creación de carpeta para generación de nuevas acreditaciones
 mkdir channel-artifacts
+#Privilegios en la carpeta /fabric-ca
+sudo chmod -R +x channel-artifacts
+
 
 #Levantamos clienta CA de nodos
 docker-compose -f docker/docker-compose-ca.yaml up -d
@@ -115,7 +133,7 @@ export PEER0_MADRID_CA=${PWD}/organizations/peerOrganizations/madrid.universidad
 export CORE_PEER_LOCALMSPID="MadridMSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_MADRID_CA
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/madrid.universidades.com/users/Admin@madrid.universidades.com/msp
-export CORE_PEER_ADDRESS=localhost:8051
+export CORE_PEER_ADDRESS=localhost:7051
 peer channel join -b ./channel-artifacts/universidadeschannel.block
 
 #Adición de nodo - Univ.Bogota
@@ -145,58 +163,18 @@ cd berlin/
 #Importamos config en formato json
 export FABRIC_CFG_PATH=$PWD
 ../../bin/configtxgen -printOrg BerlinMSP > ../organizations/peerOrganizations/berlin.universidades.com/berlin.json
+sudo chmod -R +x organizations
 
 #Levantamos nodo Univ.Berlin
 cd ..
 docker-compose -f docker/docker-compose-berlin.yaml up -d
 
-
-#Firma de la actualización con credenciales de Univ.Madrid
-#Obtenemos configuración del bloque génesis
-export PATH=${PWD}/../bin:$PATH
-export FABRIC_CFG_PATH=${PWD}/../config
+#Unión de Univ.Berlin a canal universidadeschannel
 export CORE_PEER_TLS_ENABLED=true
-export PEER0_MADRID_CA=${PWD}/organizations/peerOrganizations/madrid.universidades.com/peers/peer0.madrid.universidades.com/tls/ca.crt
-export CORE_PEER_LOCALMSPID="MadridMSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_MADRID_CA
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/madrid.universidades.com/users/Admin@madrid.universidades.com/msp
-export CORE_PEER_ADDRESS=localhost:8051
-peer channel fetch config channel-artifacts/config_block.pb -o localhost:7050 --ordererTLSHostnameOverride orderer.universidades.com -c universidadeschannel --tls --cafile ${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem
-
-
-cd channel-artifacts
-
-#Decodificación archivo binario a json
-configtxlator proto_decode --input config_block.pb --type common.Block --output config_block.json
-jq .data.data[0].payload.data.config config_block.json > config.json
-
-#Añadimos información al json del MSP del nodo Berlin en la configuración
-jq -s '.[0] * {"channel_group":{"groups":{"Application":{"groups": {"BerlinMSP":.[1]}}}}}' config.json ../organizations/peerOrganizations/berlin.universidades.com/berlin.json > modified_config.json
-
-#Actualizar configuracion definida en el json encriptandola de nuevo
-configtxlator proto_encode --input config.json --type common.Config --output config.pb
-configtxlator proto_encode --input modified_config.json --type common.Config --output modified_config.pb
-configtxlator compute_update --channel_id universidadeschannel --original config.pb --updated modified_config.pb --output berlin_update.pb
-configtxlator proto_decode --input berlin_update.pb --type common.ConfigUpdate --output berlin_update.json
-echo '{"payload":{"header":{"channel_header":{"channel_id":"'universidadeschannel'", "type":2}},"data":{"config_update":'$(cat berlin_update.json)'}}}' | jq . > berlin_update_in_envelope.json
-configtxlator proto_encode --input berlin_update_in_envelope.json --type common.Envelope --output berlin_update_in_envelope.pb
-
-#Esto define el nuevo bloque "n" en el que se encuentra la nueva config de la red.
-cd ..
-peer channel signconfigtx -f channel-artifacts/berlin_update_in_envelope.pb
-
-export PEER0_BOGOTA_CA=${PWD}/organizations/peerOrganizations/bogota.universidades.com/peers/peer0.bogota.universidades.com/tls/ca.crt
-export CORE_PEER_LOCALMSPID="BogotaMSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_BOGOTA_CA
-export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/bogota.universidades.com/users/Admin@bogota.universidades.com/msp
-export CORE_PEER_ADDRESS=localhost:9051
-peer channel update -f channel-artifacts/berlin_update_in_envelope.pb -c universidadeschannel -o localhost:7050 --ordererTLSHostnameOverride orderer.universidades.com --tls --cafile ${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem
-
+export FABRIC_CFG_PATH=${PWD}/../config
 export PEER0_BERLIN_CA=${PWD}/organizations/peerOrganizations/berlin.universidades.com/peers/peer0.berlin.universidades.com/tls/ca.crt
 export CORE_PEER_LOCALMSPID="BerlinMSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_BERLIN_CA
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/berlin.universidades.com/users/Admin@berlin.universidades.com/msp
 export CORE_PEER_ADDRESS=localhost:2051
-peer channel fetch 0 channel-artifacts/universidadeschannel.block -o localhost:7050 --ordererTLSHostnameOverride orderer.universidades.com -c universidadeschannel --tls --cafile ${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem
 peer channel join -b channel-artifacts/universidadeschannel.block
-
