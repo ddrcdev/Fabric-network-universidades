c13393797617089c326057d5e2a94309f171a991
ddrcdev <david.del.rio19@gmail.com>
2023-06-25 13:31:26 +0200

    v0

---
+++ config-univ-network.sh
@@ -0,0 +1,171 @@
+#######################################
+########## REQUISITOS PREVIOS #########
+#######################################
+
+#Comprobación de versiones
+git version
+curl version
+
+#Descarga archivos docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+#Instalación de docker
+sudo sh get-docker.sh
+docker version 
+
+#Instalación de docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
+
+#Instalación de Go 
+sudo apt install golang-go
+
+#Instalación de Node.js
+sudo apt install npm
+
+#Definición de privilegios de usuario - reiniciar consola para hacerlo efectivo
+sudo usermod -aG docker $USER
+
+#Clonación de repositorio
+git clone https://gitlab.com/STorres17/soluciones-blockchain.git
+
+#Instalación en repositorio github
+cd soluciones-blockchain/universidades
+
+#Instalación de jq - para archivos json 
+sudo apt install jq
+
+#######################################
+###### ELIMINAR CONFIG. PREVIAS #######
+#######################################
+
+#Reinicio de contenedores
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker volume prune
+docker network prune
+
+#Eliminar acreditaciones generadas
+rm -rf organizations/peerOrganizations
+rm -rf organizations/ordererOrganizations
+sudo rm -rf organizations/fabric-ca/madrid/
+sudo rm -rf organizations/fabric-ca/bogota/
+sudo rm -rf organizations/fabric-ca/ordererOrg/
+rm -rf channel-artifacts/
+
+#Creación de carpeta para generación de nuevas acreditaciones
+mkdir channel-artifacts
+
+docker-compose -f docker/docker-compose-ca.yaml up -d
+
+
+########################################
+###### GENERACIÓN DE CREDENCIALES ######
+########################################
+
+. ./organizations/fabric-ca/registerEnroll.sh && createMadrid
+. ./organizations/fabric-ca/registerEnroll.sh && createBogota
+. ./organizations/fabric-ca/registerEnroll.sh && createOrderer
+
+
+#Despliegue de red con canales 1 y 2 
+docker-compose -f docker/docker-compose-universidades.yaml up -d
+
+
+#######################################
+##### CANAL: universidadeschannel #####
+#######################################
+
+export FABRIC_CFG_PATH=${PWD}/configtx
+configtxgen -profile UniversidadesGenesis -outputBlock ./channel-artifacts/universidadeschannel.block -channelID universidadeschannel
+export FABRIC_CFG_PATH=${PWD}/../config
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem
+export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/server.crt
+export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/server.key
+
+osnadmin channel join --channelID universidadeschannel --config-block ./channel-artifacts/universidadeschannel.block -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
+osnadmin channel list -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
+
+#Adición de nodo - Univ.Madrid
+export CORE_PEER_TLS_ENABLED=true
+export PEER0_MADRID_CA=${PWD}/organizations/peerOrganizations/madrid.universidades.com/peers/peer0.madrid.universidades.com/tls/ca.crt
+export CORE_PEER_LOCALMSPID="MadridMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_MADRID_CA
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/madrid.universidades.com/users/Admin@madrid.universidades.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+peer channel join -b ./channel-artifacts/universidadeschannel.block
+
+#Adición de nodo - Univ.Bogota
+export PEER0_BOGOTA_CA=${PWD}/organizations/peerOrganizations/bogota.universidades.com/peers/peer0.bogota.universidades.com/tls/ca.crt
+export CORE_PEER_LOCALMSPID="BogotaMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_BOGOTA_CA
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/bogota.universidades.com/users/Admin@bogota.universidades.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+peer channel join -b ./channel-artifacts/universidadeschannel.block
+
+#######################################
+####### AÑADIR NODO UNIV.BERLIN #######
+#######################################
+
+#Generación certificados Univ.Berlin (tercer nodo)
+
+
+#Configuración nodo Univ.Berlin
+cd berlin/
+export FABRIC_CFG_PATH=$PWD
+../../bin/configtxgen -printOrg BerlinMSP > ../organizations/peerOrganizations/berlin.universidades.com/berlin.json
+
+#Levantamos nodo Univ.Berlin
+cd ..
+docker-compose -f docker/docker-compose-berlin.yaml up -d
+
+#Firma de la actualización con credenciales de Univ.Madrid
+#Obtenemos configuración del bloque génesis
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/../config
+export CORE_PEER_TLS_ENABLED=true
+export PEER0_MADRID_CA=${PWD}/organizations/peerOrganizations/madrid.universidades.com/peers/peer0.madrid.universidades.com/tls/ca.crt
+export CORE_PEER_LOCALMSPID="MadridMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_MADRID_CA
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/madrid.universidades.com/users/Admin@madrid.universidades.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+peer channel fetch config channel-artifacts/config_block.pb -o localhost:7050 --ordererTLSHostnameOverride orderer.universidades.com -c universidadeschannel --tls --cafile ${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem
+
+
+cd channel-artifacts
+
+#Decodificación archivo binario a json
+configtxlator proto_decode --input config_block.pb --type common.Block --output config_block.json
+jq .data.data[0].payload.data.config config_block.json > config.json
+
+#Añadimos información al json del MSP del nodo Berlin en la configuración
+jq -s '.[0] * {"channel_group":{"groups":{"Application":{"groups": {"BerlinMSP":.[1]}}}}}' config.json ../organizations/peerOrganizations/berlin.universidades.com/berlin.json > modified_config.json
+
+#Actualizar configuracion definida en el json encriptandola de nuevo
+configtxlator proto_encode --input config.json --type common.Config --output config.pb
+configtxlator proto_encode --input modified_config.json --type common.Config --output modified_config.pb
+configtxlator compute_update --channel_id universidadeschannel --original config.pb --updated modified_config.pb --output berlin_update.pb
+configtxlator proto_decode --input berlin_update.pb --type common.ConfigUpdate --output berlin_update.json
+echo '{"payload":{"header":{"channel_header":{"channel_id":"'universidadeschannel'", "type":2}},"data":{"config_update":'$(cat berlin_update.json)'}}}' | jq . > berlin_update_in_envelope.json
+configtxlator proto_encode --input berlin_update_in_envelope.json --type common.Envelope --output berlin_update_in_envelope.pb
+
+#Esto define el nuevo bloque "n" en el que se encuentra la nueva config de la red.
+cd ..
+peer channel signconfigtx -f channel-artifacts/berlin_update_in_envelope.pb
+
+export PEER0_BOGOTA_CA=${PWD}/organizations/peerOrganizations/bogota.universidades.com/peers/peer0.bogota.universidades.com/tls/ca.crt
+export CORE_PEER_LOCALMSPID="BogotaMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_BOGOTA_CA
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/bogota.universidades.com/users/Admin@bogota.universidades.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+peer channel update -f channel-artifacts/berlin_update_in_envelope.pb -c universidadeschannel -o localhost:7050 --ordererTLSHostnameOverride orderer.universidades.com --tls --cafile ${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem
+
+export PEER0_BERLIN_CA=${PWD}/organizations/peerOrganizations/berlin.universidades.com/peers/peer0.berlin.universidades.com/tls/ca.crt
+export CORE_PEER_LOCALMSPID="BerlinMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_BERLIN_CA
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/berlin.universidades.com/users/Admin@berlin.universidades.com/msp
+export CORE_PEER_ADDRESS=localhost:2051
+peer channel fetch 0 channel-artifacts/universidadeschannel.block -o localhost:7050 --ordererTLSHostnameOverride orderer.universidades.com -c universidadeschannel --tls --cafile ${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem
+peer channel join -b channel-artifacts/universidadeschannel.block
+
---
+++ universidades/organizations/fabric-ca/registerEnroll.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+function createUniversity(NAME,PORT) {
+  infoln "Enrolling the CA admin"
+  mkdir -p organizations/peerOrganizations/${NAME}.universidades.com/
+
+  export FABRIC_CA_CLIENT_HOME=${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/
+
+  set -x
+  fabric-ca-client enroll -u https://admin:adminpw@localhost:${PORT} --caname ca-${NAME} --tls.certfiles "${PWD}/organizations/fabric-ca/${NAME}/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  echo 'NodeOUs:
+  Enable: true
+  ClientOUIdentifier:
+    Certificate: cacerts/localhost-${PORT}-ca-${NAME}.pem
+    OrganizationalUnitIdentifier: client
+  PeerOUIdentifier:
+    Certificate: cacerts/localhost-${PORT}-ca-${NAME}.pem
+    OrganizationalUnitIdentifier: peer
+  AdminOUIdentifier:
+    Certificate: cacerts/localhost-${PORT}-ca-${NAME}.pem
+    OrganizationalUnitIdentifier: admin
+  OrdererOUIdentifier:
+    Certificate: cacerts/localhost-${PORT}-ca-${NAME}.pem
+    OrganizationalUnitIdentifier: orderer' > "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/msp/config.yaml"
+
+  infoln "Registering peer0"
+  set -x
+  fabric-ca-client register --caname ca-${NAME} --id.name peer0 --id.secret peer0pw --id.type peer --tls.certfiles "${PWD}/organizations/fabric-ca/${NAME}/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  infoln "Registering user"
+  set -x
+  fabric-ca-client register --caname ca-${NAME} --id.name user1 --id.secret user1pw --id.type client --tls.certfiles "${PWD}/organizations/fabric-ca/${NAME}/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  infoln "Registering the org admin"
+  set -x
+  fabric-ca-client register --caname ca-${NAME} --id.name ${NAME}admin --id.secret ${NAME}adminpw --id.type admin --tls.certfiles "${PWD}/organizations/fabric-ca/${NAME}/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  infoln "Generating the peer0 msp"
+  set -x
+  fabric-ca-client enroll -u https://peer0:peer0pw@localhost:${PORT} --caname ca-${NAME} -M "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/msp" --csr.hosts peer0.${NAME}.universidades.com --tls.certfiles "${PWD}/organizations/fabric-ca/${NAME}/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  cp "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/msp/config.yaml" "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/msp/config.yaml"
+
+  infoln "Generating the peer0-tls certificates"
+  set -x
+  fabric-ca-client enroll -u https://peer0:peer0pw@localhost:${PORT} --caname ca-${NAME} -M "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/tls" --enrollment.profile tls --csr.hosts peer0.${NAME}.universidades.com --csr.hosts localhost --tls.certfiles "${PWD}/organizations/fabric-ca/${NAME}/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  cp "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/tls/tlscacerts/"* "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/tls/ca.crt"
+  cp "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/tls/signcerts/"* "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/tls/server.crt"
+  cp "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/tls/keystore/"* "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/tls/server.key"
+
+  mkdir -p "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/msp/tlscacerts"
+  cp "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/tls/tlscacerts/"* "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/msp/tlscacerts/ca.crt"
+
+  mkdir -p "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/tlsca"
+  cp "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/tls/tlscacerts/"* "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/tlsca/tlsca.${NAME}.universidades.com-cert.pem"
+
+  mkdir -p "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/ca"
+  cp "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/peers/peer0.${NAME}.universidades.com/msp/cacerts/"* "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/ca/ca.${NAME}.universidades.com-cert.pem"
+
+  infoln "Generating the user msp"
+  set -x
+  fabric-ca-client enroll -u https://user1:user1pw@localhost:${PORT} --caname ca-${NAME} -M "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/users/User1@${NAME}.universidades.com/msp" --tls.certfiles "${PWD}/organizations/fabric-ca/${NAME}/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  cp "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/msp/config.yaml" "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/users/User1@${NAME}.universidades.com/msp/config.yaml"
+
+  infoln "Generating the org admin msp"
+  set -x
+  fabric-ca-client enroll -u https://${NAME}admin:${NAME}adminpw@localhost:${PORT} --caname ca-${NAME} -M "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/users/Admin@${NAME}.universidades.com/msp" --tls.certfiles "${PWD}/organizations/fabric-ca/${NAME}/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  cp "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/msp/config.yaml" "${PWD}/organizations/peerOrganizations/${NAME}.universidades.com/users/Admin@${NAME}.universidades.com/msp/config.yaml"
+}
+
+
+
+function createOrderer() {
+  infoln "Enrolling the CA admin"
+  mkdir -p organizations/ordererOrganizations/universidades.com
+
+  export FABRIC_CA_CLIENT_HOME=${PWD}/organizations/ordererOrganizations/universidades.com
+
+  set -x
+  fabric-ca-client enroll -u https://admin:adminpw@localhost:7054 --caname ca-orderer --tls.certfiles "${PWD}/organizations/fabric-ca/ordererOrg/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  echo 'NodeOUs:
+  Enable: true
+  ClientOUIdentifier:
+    Certificate: cacerts/localhost-7054-ca-orderer.pem
+    OrganizationalUnitIdentifier: client
+  PeerOUIdentifier:
+    Certificate: cacerts/localhost-7054-ca-orderer.pem
+    OrganizationalUnitIdentifier: peer
+  AdminOUIdentifier:
+    Certificate: cacerts/localhost-7054-ca-orderer.pem
+    OrganizationalUnitIdentifier: admin
+  OrdererOUIdentifier:
+    Certificate: cacerts/localhost-7054-ca-orderer.pem
+    OrganizationalUnitIdentifier: orderer' > "${PWD}/organizations/ordererOrganizations/universidades.com/msp/config.yaml"
+
+  infoln "Registering orderer"
+  set -x
+  fabric-ca-client register --caname ca-orderer --id.name orderer --id.secret ordererpw --id.type orderer --tls.certfiles "${PWD}/organizations/fabric-ca/ordererOrg/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  infoln "Registering the orderer admin"
+  set -x
+  fabric-ca-client register --caname ca-orderer --id.name ordererAdmin --id.secret ordererAdminpw --id.type admin --tls.certfiles "${PWD}/organizations/fabric-ca/ordererOrg/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  infoln "Generating the orderer msp"
+  set -x
+  fabric-ca-client enroll -u https://orderer:ordererpw@localhost:7054 --caname ca-orderer -M "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp" --csr.hosts orderer.universidades.com --csr.hosts localhost --tls.certfiles "${PWD}/organizations/fabric-ca/ordererOrg/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  cp "${PWD}/organizations/ordererOrganizations/universidades.com/msp/config.yaml" "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/config.yaml"
+
+  infoln "Generating the orderer-tls certificates"
+  set -x
+  fabric-ca-client enroll -u https://orderer:ordererpw@localhost:7054 --caname ca-orderer -M "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls" --enrollment.profile tls --csr.hosts orderer.universidades.com --csr.hosts localhost --tls.certfiles "${PWD}/organizations/fabric-ca/ordererOrg/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  cp "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/tlscacerts/"* "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/ca.crt"
+  cp "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/signcerts/"* "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/server.crt"
+  cp "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/keystore/"* "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/server.key"
+
+  mkdir -p "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts"
+  cp "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/tlscacerts/"* "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem"
+
+  mkdir -p "${PWD}/organizations/ordererOrganizations/universidades.com/msp/tlscacerts"
+  cp "${PWD}/organizations/ordererOrganizations/universidades.com/orderers/orderer.universidades.com/tls/tlscacerts/"* "${PWD}/organizations/ordererOrganizations/universidades.com/msp/tlscacerts/tlsca.universidades.com-cert.pem"
+
+  infoln "Generating the admin msp"
+  set -x
+  fabric-ca-client enroll -u https://ordererAdmin:ordererAdminpw@localhost:7054 --caname ca-orderer -M "${PWD}/organizations/ordererOrganizations/universidades.com/users/Admin@universidades.com/msp" --tls.certfiles "${PWD}/organizations/fabric-ca/ordererOrg/tls-cert.pem"
+  { set +x; } 2>/dev/null
+
+  cp "${PWD}/organizations/ordererOrganizations/universidades.com/msp/config.yaml" "${PWD}/organizations/ordererOrganizations/universidades.com/users/Admin@universidades.com/msp/config.yaml"
+}
